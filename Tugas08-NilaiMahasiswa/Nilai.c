279bb1eac14c01dd47c7b684d41d9216a67f3a5a
sanbouncer <146912590+sanbouncer@users.noreply.github.com>
2023-11-22 10:20:32 +0700

    Update Nilai.c

--- Tugas08-NilaiMahasiswa/Nilai.c
+++ Tugas08-NilaiMahasiswa/Nilai.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 float tugas, quiz, uts, uas, final;
-int mutu;
-char nama[50], nim[12];
+char nama[50], nim[20], mutu[3];
 
 void calcNilai() {
     final = (0.1 * tugas) + (0.2 * quiz) + (0.3 * uts) + (0.4 * uas);
 
     if (final > 90) {
-        mutu = 'A';
+        strcpy(mutu, "A");
     } else if (final >= 81 && final <= 90) {
-        mutu = 'AB';
+        strcpy(mutu, "AB");
     } else if (final >= 71 && final <= 80) {
-        mutu = 'B';
+        strcpy(mutu, "B");
     } else if (final >= 61 && final <= 70) {
-        mutu = 'BC';
+        strcpy(mutu, "BC");
     }else if (final >= 51 && final <= 60) {
-        mutu = 'C';
+        strcpy(mutu, "C");
     }else if (final >= 45 && final <= 50) {
-        mutu = 'D';
+        strcpy(mutu, "D");
     } else if (final <= 45) {
-        mutu = 'B';
+        strcpy(mutu, "E");
     }
 }
 
@@ -29,16 +29,16 @@ void displayNilaiAkhir() {
     printf("Nama Mahasiswa\t: %s\n", nama);
     printf("NIM\t\t: %s\n", nim);
     printf("Nilai Akhir\t: %.2f\n", final);
-    printf("Mutu\t\t: %c\n", mutu);
+    printf("Mutu\t\t: %s\n", mutu);
 
-    if (mutu != 'E' && mutu != 'D') {
+    if (strcmp(mutu, "E") != 0 && strcmp(mutu, "D") != 0) {
         printf("Lulus\n");
     } else {
         printf("Tidak Lulus\n");
     }
 }
 
-void main() {
+int main() {
     printf("Nama Mahasiswa\t: ");
     scanf("%s", nama);
     printf("NIM\t\t: ");
@@ -55,4 +55,5 @@ void main() {
     calcNilai();
     displayNilaiAkhir();
 
+    return 0;
 }
